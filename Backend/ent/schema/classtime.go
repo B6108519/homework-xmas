9aa68fb15ee99f3db1cfb96e3a80c9417206d52f
Arclight <skytekking.art@gmail.com>
2020-12-27 20:33:01 +0700

    ลองฝึกเล่นๆ -close #5

---
+++ Backend/ent/schema/classtime.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Classtime holds the schema definition for the Classtime entity.
+type Classtime struct {
+	ent.Schema
+}
+
+// Fields of the Classtime.
+func (Classtime) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Classtime.
+func (Classtime) Edges() []ent.Edge {
+	return nil
+}
